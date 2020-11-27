5633225340a6be090ef823fae76a641578156a92
kamilwegrzyn <48628515+kamilwegrzyn@users.noreply.github.com>
2020-11-27 23:49:20 +0100

    lab3
    lab3 added

---
+++ lab3/Wsei.ExchangeThings.Web/Controllers/ItemController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Wsei.ExchangeThings.Web.Models;
+
+namespace Wsei.ExchangeThings.Web.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ItemController : ControllerBase
+    {
+        public AddNewItemResponseModel Post(ItemModel item) {
+            bool s = !string.IsNullOrEmpty(item.Description) && !string.IsNullOrEmpty(item.Name);
+            string message = s ? "" : "Input fields cannot be empty";
+            var res = new AddNewItemResponseModel
+            {
+                success = s,
+                message = s ? "" : message
+            };
+            return res;
+        }
+    }
+}
