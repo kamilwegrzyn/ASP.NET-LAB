dbcd052fec1cea44ffbee98cff1cb598763a3658
Kamil Węgrzyn <48628515+kamilwegrzyn@users.noreply.github.com>
2020-12-11 16:25:51 +0100

    lab1,lab2 deleted from lab3 branch

--- lab2/Wsei.ExchangeThings.Web/Models/AddNewItemConfirmationViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Wsei.ExchangeThings.Web.Models
-{
-    public class AddNewItemConfirmationViewModel
-    {
-        public int Id { get; internal set; }
-
-        public string Name { get; internal set; }
-    }
-}
