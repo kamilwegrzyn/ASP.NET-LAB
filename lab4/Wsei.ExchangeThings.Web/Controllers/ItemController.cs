b0b78ac3bd03f7f8dfdf17d0bd394c514a44de9d
Kamil Węgrzyn <48628515+kamilwegrzyn@users.noreply.github.com>
2020-12-11 16:09:21 +0100

    lab4 added

---
+++ lab4/Wsei.ExchangeThings.Web/Controllers/ItemController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Wsei.ExchangeThings.Web.Database;
+using Wsei.ExchangeThings.Web.Entities;
+using Wsei.ExchangeThings.Web.Models;
+
+namespace Wsei.ExchangeThings.Web.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ItemController : ControllerBase
+    {
+        private readonly ExchangesDbContext _dbContext;
+
+        public ItemController(ExchangesDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public AddNewItemResponseModel<DbSet<ItemEntity>> Post(ItemModel item) {
+            bool isValid = !string.IsNullOrEmpty(item.Description) && !string.IsNullOrEmpty(item.Name);
+            string message = isValid ? "" : "Input fields cannot be empty";
+
+            if(isValid)
+            {
+                var entity = new ItemEntity
+                {
+                    Name = item.Name,
+                    Description = item.Description,
+                    IsVisible = item.IsVisible
+                };
+
+                _dbContext.Items.Add(entity);
+                _dbContext.SaveChanges();
+            }
+
+            var res = new AddNewItemResponseModel<DbSet<ItemEntity>>
+            {
+                success = isValid,
+                message = message,
+                data = isValid ? _dbContext.Items : null
+            };
+
+            return res;
+        }
+    }
+}
---
+++ lab4/Wsei.ExchangeThings.Web/wwwroot/js/addNewItem.js
@@ -0,0 +1,48 @@
+﻿(function () {
+    const alertElement = document.getElementById("success-alert");
+    const errorAlertElement = document.getElementById("error-alert");
+    const list = document.getElementById("list");
+    const msg = document.getElementById("msg");
+    const form = document.forms[0];
+    const addNewItem = async () => {
+        // 1. read data from the form
+        const requestData = JSON.stringify({ Name: document.getElementById("Name").value, Description: document.getElementById("Description").value, IsVisible: document.getElementById("IsVisible").checked });
+        // 2. call the application server using fetch method
+        const url = "/api/item";
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: requestData
+        });
+        const responseJson = await response.json();
+        if (responseJson.success) {
+            alertElement.style.display = "";
+            var HTML = "<table border=1 width=100%><tr><th>Name</th><th>Description</th><th>Visibility</th></tr>";
+
+            responseJson.data.forEach(element => {
+                HTML += "<tr><td align=center>" + element.name + "</td>";
+                HTML += "<td align=center>" + element.description + "</td>";
+                HTML += "<td align=center>" + element.isVisible + "</td></tr>";
+            });
+
+            HTML += "</table>";
+
+            list.innerHTML = HTML;
+            list.style.display =  "";
+        } else {
+            errorAlertElement.style.display = "";
+            msg.innerText = responseJson.message;
+        }
+    };
+    window.addEventListener("load", () => {
+        form.addEventListener("submit", event => {
+            event.preventDefault();
+            alertElement.style.display = "none";
+            errorAlertElement.style.display = "none";
+            list.style.display = "none";
+            addNewItem().then(() => console.log("added successfully"));
+        });
+    });
+})();
